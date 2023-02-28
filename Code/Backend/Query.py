1321f36f8e9f10c1138a54aeeb392409057af5ca
Zhijun Zeng <88609108+HeyitsOrion@users.noreply.github.com>
2023-02-28 12:31:04 +0000

    fix some bugs on phrase_search

--- Code/Backend/Query.py
+++ Code/Backend/Query.py
@@ -159,7 +159,8 @@ class Query:
                             for position2 in dict2[doic]:
                                 if not position2.isdigit():
                                     continue
-                                if int(position2) - int(position1) == 1 or int(position2) - int(position1) == 0:
+                                distance = abs(int(position2) - int(position1))
+                                if distance <2 :
                                     result.append(doic)
                     if result:
                         return list(dict.fromkeys(result))
