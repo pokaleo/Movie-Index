9f4cc2876a01834969e45dba79b1a34f594ed2db
ElinorDing <47607489+ElinorDing@users.noreply.github.com>
2023-03-07 04:46:44 +0000

    update web-scraping

--- Code/Backend/webScrap.py
+++ Code/Backend/webScrap.py
@@ -4,7 +4,12 @@ from bs4 import BeautifulSoup
 from collections import defaultdict
 import re
 from dict2xml import dict2xml
+from dicttoxml import dicttoxml
+from xml.dom.minidom import parseString
 import os
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element,tostring
+import xml.dom.minidom
 
 
 class webScraping:
@@ -42,65 +47,73 @@ class webScraping:
 
 
     def certainResponse(self, container, curr_class, curr_str):
-        res = defaultdict(str)
+        # res = defaultdict(str)
+        res = []
         if curr_str == 'runtime':
             temp = container.p.find(curr_class, class_ = curr_str)
             if temp == None:
                 return None
             else:
-                res['runningtime'] = container.p.find(curr_class, class_ = curr_str).text.replace(' min','')
+                res.append(container.p.find(curr_class, class_ = curr_str).text.replace(' min',''))
                 return res
         elif curr_str == 'certificate':
-            temp = container.p.find(curr_class, class_ = curr_str)
-            if temp == None:
-                return None
-            else:
-                res['certificate'] = container.p.find(curr_class, class_ = curr_str).text
-                return res
+            data_cer = container.find(curr_class,attrs={'class':'ipl-zebra-list__item','id':'certifications-list'})
+            cert_ = data_cer.find_all('li',attrs={'class':'ipl-inline-list__item'})
+            for i in cert_:
+                raw_ = i.a.text.split(':')
+                res.append((raw_[0],raw_[1]))
+            return res
         elif curr_str == 'genre':
+            res = []
             temp = container.p.find(curr_class, class_ = curr_str)
             if temp == None:
                 return None
             else:
                 raw_ = container.find(curr_class, class_ = curr_str).text.replace('\n','')
-                res['genre'] = re.sub(' +', '', raw_).split(',')
-                return res
+                return re.sub(' +', '', raw_).split(',')
         elif curr_str == 'color':
+            res =[]
             temp = container.find(curr_class, attrs = {'class':'ipc-metadata-list__item','data-testid':'title-techspec_color'})
             if temp == None:
                 return None
             else:
-                res['colorinfo'] = temp.a.text
+                res.append(temp.a.text)
                 return res
         elif curr_str == 'soundmixes':
             temp = container.find(curr_class, attrs = {'class':'ipc-metadata-list__item','data-testid':'title-techspec_soundmix'})
             if temp == None:
                 return None
             else:
-                res['soundmixe'] = temp.a.text
+                res.append(temp.a.text)
                 return res
         elif curr_str == 'keywords':
             k_list = []
             data_keyword_ = container.findAll(curr_class, attrs={'class':'soda sodavote'})
             for i in data_keyword_:
                 k_list.append(i['data-item-keyword'])
-            res['keyword'] = k_list
-            return res
+            return k_list
         elif curr_str == 'country':
             data_country = container.find(curr_class, attrs = {'class':'ipc-metadata-list__item','data-testid':'title-details-origin'})
-            res[curr_str] = data_country.a.text
+            res.append(data_country.a.text)
             return res
         elif curr_str == 'releasedate':
-            res[curr_str] = container.find(curr_class, class_="ipc-inline-list__item").text
-            return res
+            date_list = []
+            data_released = container.find(curr_class, attrs = {'data-testid':'sub-section-releases','class':'sc-f65f65be-0 fVkLRr'})
+            date_ = data_released.find_all('li',attrs={'data-testid':'list-item'})
+            for i in date_:
+                date_ = ''.join(i.label.text.split(','))
+                date_list.append((i.a['aria-label'],date_))
+            return date_list
+            # res.append(container.find(curr_class, class_="ipc-inline-list__item").text)
+            # return res
         elif curr_str == 'language':
             data_language = container.find(curr_class, attrs = {'class':'ipc-metadata-list__item','data-testid':'title-details-languages'})
-            res['language'] = data_language.a.text
+            res.append(data_language.a.text)
             return res
 
 
     def getCrew(self,container,curr_class,curr_id):
-        res = defaultdict(list)
+        # res = []
         crew_ = container.find(curr_class,attrs={'class':"dataHeaderWithBorder",'id':curr_id})
         if crew_ == None:
             return None
@@ -115,12 +128,12 @@ class webScraping:
                     name_ = i.find('a').text.replace('\n','')
                     cr_list.append(name_)
 
-            res[curr_id] = cr_list
-            return res
+            # res[curr_id] = cr_list
+            return cr_list
     
 
     def geCast(self,container,curr_class):
-        res_cast = defaultdict(dict)
+        # res_cast = defaultdict(dict)
         cast_list = []
         for i in container:
             act_ = defaultdict(str)
@@ -128,8 +141,8 @@ class webScraping:
             act_['actor'] = i.find(curr_class, attrs={'data-testid':'title-cast-item__actor'}).text
             char_['role'] = i.find(curr_class,attrs={'data-testid':'cast-item-characters-link'}).span.text
             cast_list.append((act_, char_))
-        res_cast['credit'] = cast_list
-        return res_cast
+        # res_cast['credit'] = cast_list
+        return cast_list
     
 
     def getResponse(self):
@@ -138,64 +151,6 @@ class webScraping:
         soup = BeautifulSoup(response.content, 'html.parser')
         data_div = soup.findAll('div', attrs={'class':'lister-item mode-advanced'})
         count = 0
-
-        # TEST CASE
-        # container = data_div[0]
-        # curr_info = defaultdict(str)
-        # raw_id = container.h3.a['href']
-        # curr_id = raw_id.split('/')[2]
-        # self.doc_id.append(curr_id)
-        # curr_info['docid'] = curr_id
-        # curr_info['title'] = container.h3.a.text
-        # curr_info['year'] = container.h3.find('span', class_ = 'lister-item-year text-muted unbold').text.replace('(', '').replace(')', '')
-
-        # all_in_text_muted = container.find_all('p', class_ = 'text-muted')
-        # curr_info['plot'] = all_in_text_muted[1].text.replace('\n', '') if len(all_in_text_muted) >1 else '*****'
-        
-        # curr_info['runningtimes'] = self.certainResponse(container,'span','runtime')
-        # curr_info['certificates'] = self.certainResponse(container,'span','certificate')
-        # curr_info['genres'] = self.certainResponse(container,'span','genre')
-
-        # # Access to the detail page
-        # curr_ = self.url_baseline + '/title/'+curr_id+'/'   
-        # req = requests.get(url= curr_, headers={'User-Agent': 'Mozilla/5.0'}).text
-        # curr_page = BeautifulSoup(req,'html.parser')
-        # data_detail = curr_page.find('div', attrs = {'data-testid':'title-details-section','class':'sc-f65f65be-0 fVkLRr'})
-        # curr_info['releasedates'] = self.certainResponse(data_detail,'li', 'releasedate')
-        # curr_info['countries'] = self.certainResponse(data_detail,'li', 'country')
-        # curr_info['languages'] = self.certainResponse(data_detail,'li', 'language')
-
-        # data_tech = curr_page.find('div', attrs = {'data-testid':'title-techspecs-section','class':'sc-f65f65be-0 fVkLRr'})
-        # curr_info['colorinfos'] = self.certainResponse(data_tech,'li','color')
-        # curr_info['soundmixes'] = self.certainResponse(data_tech,'li','soundmixes')
-
-        # data_cast = curr_page.findAll('div', attrs={'data-testid':'title-cast-item','class':'sc-bfec09a1-5 kUzsHJ'})
-        # curr_info['cast'] = self.geCast(data_cast,'a')
-        
-        # data_type = curr_page.find('meta',attrs={'property':{'og:type'}})
-        # curr_info['type'] = data_type['content'].split('.')[1]
-
-
-        # keywords_ = self.url_baseline + '/title/'+curr_id+'/keywords?ref_=tt_stry_kw'
-        # req_k = requests.get(url= keywords_, headers={'User-Agent': 'Mozilla/5.0'}).text
-        # k_soup = BeautifulSoup(req_k,'html.parser')
-
-        # curr_info['keywords'] = self.certainResponse(k_soup,'td','keywords')
-
-        # cast_crew_ = self.url_baseline + '/title/'+curr_id+'/fullcredits/?ref_=tt_cl_sm'
-        # req_c = requests.get(url= cast_crew_, headers={'User-Agent': 'Mozilla/5.0'}).text
-        # c_soup = BeautifulSoup(req_c,'html.parser')
-
-        # curr_info['directors'] = self.getCrew(c_soup,'h4','director') 
-        # curr_info['composers'] = self.getCrew(c_soup,'h4','composer')
-        # curr_info['producers'] = self.getCrew(c_soup,'h4','producer')
-        # curr_info['writers'] = self.getCrew(c_soup,'h4','writer')
-        # curr_info['editors'] = self.getCrew(c_soup,'h4','editor')
-
-        # return curr_info
-
-
-        # ACTUAL CODE
 
         for container in data_div:
             print(count)
@@ -208,7 +163,7 @@ class webScraping:
             curr_info['title'] = container.h3.a.text
             curr_info['year'] = container.h3.find('span', class_ = 'lister-item-year text-muted unbold').text.replace('(', '').replace(')', '')
             curr_info['runningtimes'] = self.certainResponse(container,'span','runtime')
-            curr_info['certificates'] = self.certainResponse(container,'span','certificate')
+            # curr_info['certificates'] = self.certainResponse(container,'span','certificate')
             curr_info['genres'] = self.certainResponse(container,'span','genre')
             all_in_text_muted = container.find_all('p', class_ = 'text-muted')
             curr_info['plot'] = all_in_text_muted[1].text.replace('\n', '') if len(all_in_text_muted) >1 else '*****'
@@ -218,14 +173,12 @@ class webScraping:
             req = requests.get(url= curr_, headers={'User-Agent': 'Mozilla/5.0'}).text
             curr_page = BeautifulSoup(req,'html.parser')
             data_detail = curr_page.find('div', attrs = {'data-testid':'title-details-section','class':'sc-f65f65be-0 fVkLRr'})
-            curr_info['releasedates'] = data_detail.find('li', class_="ipc-inline-list__item").text
-            data_country = data_detail.find('li', attrs = {'class':'ipc-metadata-list__item','data-testid':'title-details-origin'})
-            curr_info['countries'] = data_country.a.text
-            data_language = data_detail.find('li', attrs = {'class':'ipc-metadata-list__item','data-testid':'title-details-languages'})
-            curr_info['language'] = data_language.a.text
+
+            curr_info['countries'] = self.certainResponse(data_detail,'li','country')
+            curr_info['languages'] = self.certainResponse(data_detail,'li','language')
 
             data_tech = curr_page.find('div', attrs = {'data-testid':'title-techspecs-section','class':'sc-f65f65be-0 fVkLRr'})
-            curr_info['colorinfo'] = self.certainResponse(data_tech,'li','color')
+            curr_info['colorinfos'] = self.certainResponse(data_tech,'li','color')
             curr_info['soundmixes'] = self.certainResponse(data_tech,'li','soundmixes')
 
             data_cast = curr_page.findAll('div', attrs={'data-testid':'title-cast-item','class':'sc-bfec09a1-5 kUzsHJ'})
@@ -234,6 +187,18 @@ class webScraping:
             data_type = curr_page.find('meta',attrs={'property':{'og:type'}})
             curr_info['type'] = data_type['content'].split('.')[1]
 
+
+            releaseInfo_ = self.url_baseline + '/title/'+curr_id+'/releaseinfo/?ref_=tt_dt_rdat'
+            req_re = requests.get(url= releaseInfo_, headers={'User-Agent': 'Mozilla/5.0'}).text
+            re_soup = BeautifulSoup(req_re,'html.parser')
+
+            curr_info['releasedates'] = self.certainResponse(re_soup,'div','releasedate')
+
+            certificateInfo_ = self.url_baseline + '/title/'+curr_id+'/parentalguide?ref_=tt_stry_pg'
+            req_cer = requests.get(url= certificateInfo_, headers={'User-Agent': 'Mozilla/5.0'}).text
+            cer_soup = BeautifulSoup(req_cer,'html.parser')
+
+            curr_info['certificates'] = self.certainResponse(cer_soup,'tr','certificate')
 
             keywords_ = self.url_baseline + '/title/'+curr_id+'/keywords?ref_=tt_stry_kw'
             req_k = requests.get(url= keywords_, headers={'User-Agent': 'Mozilla/5.0'}).text
@@ -245,29 +210,63 @@ class webScraping:
             req_c = requests.get(url= cast_crew_, headers={'User-Agent': 'Mozilla/5.0'}).text
             c_soup = BeautifulSoup(req_c,'html.parser')
 
-            curr_info['director'] = self.getCrew(c_soup,'h4','director') 
-            curr_info['composer'] = self.getCrew(c_soup,'h4','composer')
-            curr_info['producer'] = self.getCrew(c_soup,'h4','producer')
-            curr_info['writer'] = self.getCrew(c_soup,'h4','writer')
-            curr_info['editor'] = self.getCrew(c_soup,'h4','editor')     
+            curr_info['directors'] = self.getCrew(c_soup,'h4','director') 
+            curr_info['composers'] = self.getCrew(c_soup,'h4','composer')
+            curr_info['producers'] = self.getCrew(c_soup,'h4','producer')
+            curr_info['writers'] = self.getCrew(c_soup,'h4','writer')
+            curr_info['editors'] = self.getCrew(c_soup,'h4','editor')     
             
 
-            print(curr_info)
+            # print(curr_info)
             self.allInfo[curr_id] = curr_info
 
-    def toXML(self):
-        for k, v in self.allInfo.items():
-            id = k
-            file_name = id + '.xml'
-            res_xml= dict2xml(v, 'doc')
-            m_encoding = 'utf-8'
-
-            with open(os.path.join('../Dataset/IMDB TestData',file_name), 'w') as xfile:
-                xfile.write('<?xml version="1.0" encoding=\"{}\"?>\n'.format(m_encoding))
-                xfile.write(res_xml)
-                xfile.close()
-
+    def dict2XML(self):
+        for id, values in self.allInfo.items():
+            # Create the root element
+            root = ET.Element('doc', {'id': id})
             
+            # Loop over each value in the inner defaultdict
+            for key, value in values.items():
+                # If the value is a list, loop over it and create a separate XML element for each item
+                if isinstance(value, list):
+                    sub = ET.SubElement(root, key)  
+                    if key == 'runningtimes':
+                        for item in value:
+                            ET.SubElement(sub, key[:-1],{'country':"default"}).text = item
+                    elif key == 'certificates':
+                        for item in value:
+                            ET.SubElement(sub, key[:-1],{'country':item[0]}).text = item[1]
+                    elif key == 'releasedates':
+                        for item in value:
+                            ET.SubElement(sub, key[:-1],{'country':item[0]}).text = item[1]
+                    elif key == 'cast':
+                        for actor_dict, role_dict in value:
+                            credit = ET.SubElement(sub, 'credit')
+                            actor = ET.SubElement(credit, 'actor')
+                            actor.text = actor_dict['actor']
+                            role = ET.SubElement(credit, 'role')
+                            role.text = role_dict['role']
+                    elif key == 'countries':
+                        for item in value:
+                            ET.SubElement(sub, 'country').text = item
+                    elif key == 'soundmixes':
+                        for item in value:
+                            ET.SubElement(sub, 'soundmix').text = item
+                    else:
+                        for item in value:
+                            ET.SubElement(sub, key[:-1]).text = item
+                else:
+                    ET.SubElement(root, key).text = value
+            
+            # Create the XML tree and write it to a file with the id as its filename
+            tree = ET.ElementTree(root)
+            xmlstr = xml.dom.minidom.parseString(ET.tostring(root)).toprettyxml(indent="\t")
+
+            with open(os.path.join('../Dataset/IMDB TestData', id+'.xml'), "w", encoding="utf-8") as f:
+                f.write(xmlstr)
+
+
+
 
 
 
@@ -280,8 +279,8 @@ if __name__ == '__main__':
     args = parser.parse_args()
     webScrap = webScraping(args.url, args.url_baseline)
 
-    a = webScrap.getResponse()
-    webScrap.toXML()
+    webScrap.getResponse()
+    webScrap.dict2XML()
 
 
 # command line: 
